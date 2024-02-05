233c6c89c7f821725b7fb0f48baa116f8d0108b6
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-05 19:08:46 +0530

    render contentful posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,19 +4,23 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 import blogStyles from './blog.module.scss'
 
+//
+// Goal: Render Contentful Posts
+// 
+// 1. Swap out the markdown query with the contentful query
+// 2. Update the component to render the new data
+//    - Don't worry if the link links to a non-existant page
+// 3. Test your work!
+
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost ( sort: { fields: publishedDate, order: DESC } ) {
                 edges {
                     node {
-                        frontmatter {
-                            title
-                            date
-                        }
-                        fields {
-                            slug
-                        }
+                        title
+                        slug
+                        publishedDate(formatString:"MMMM Do, YYYY")
                     }
                 }
             }
@@ -27,12 +31,12 @@ const BlogPage = () => {
         <Layout>
             <h1>Blog</h1>
             <ol className={blogStyles.posts}>
-                {data.allMarkdownRemark.edges.map((edge) => {
+                {data.allContentfulBlogPost.edges.map((edge) => {
                     return (
                         <li className={blogStyles.post}>
-                            <Link to={`/blog/${edge.node.fields.slug}`}>
-                                <h2>{edge.node.frontmatter.title}</h2>
-                                <p>{edge.node.frontmatter.date}</p>
+                            <Link to={`/blog/${edge.node.slug}`}>
+                                <h2>{edge.node.title}</h2>
+                                <p>{edge.node.publishedDate}</p>
                             </Link>
                         </li>
                     )
