ebaf9ff28d239df018de50594c6943e2183adc8c
Darshan Chobarkar <dchobarkar@gmail.com>
2024-02-05 18:04:23 +0530

    add page links

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I currently teach full-time on my blog.</p>
+      <p>
+        <Link to="/contact">Want to work with me? Reach out.</Link>
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        The best way to reach me is via{" "}
+        <a href="https://twitter.com/@barbatos__008" target="_blank">
+          @barbatos__008
+        </a>{" "}
+        on Twitter!
+      </p>
+    </div>
+  );
+};
+
+export default ContactPage;
